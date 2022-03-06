48fab3d84243d77951e9740349227b0e759f2080
SergZu <sergzu0@gmail.com>
2022-03-07 03:01:24 +0600

    Initial config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const dotenv = require('dotenv').config( {
+	path: path.join(__dirname, '.env')
+  } );
+const definePluginConfig = new webpack.DefinePlugin({
+    "process.env": dotenv.parsed
+  });
 
 module.exports = {
     entry: path.join(__dirname, "src", "index.tsx"),
@@ -14,6 +21,23 @@ module.exports = {
             use: 'ts-loader',
             exclude: /node_modules/,
           },
+          {
+            test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+            exclude: /node_modules/,
+            type: 'asset/resource',
+        },
+
+        {
+            test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+            exclude: /node_modules/,
+            type: 'asset/inline',
+        },
+
+        {
+            test: /\.(scss|css)$/,
+            exclude: /node_modules/,
+            use: ['style-loader', 'css-loader'], 
+        },
         ],
       },
     resolve: { 
@@ -24,8 +48,9 @@ module.exports = {
         static: path.join(__dirname, "src") 
     },
     plugins: [
+        definePluginConfig,
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
-        }),
+        })
     ],
 };
